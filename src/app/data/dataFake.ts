828a158f3862094e9c0c3821264a76cd9e68870c
KasnorDKS <junior.castro10@hotmail.com>
2023-10-16 18:26:24 -0300

    Adjusted components for better visual

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,13 +3,13 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "FRAMEWORK ANGULAR NOVA VERSÃO",
-    "description": "Angular atualiza para a nova versão 15 e trás novas funcionalidades.",
+    "title": "Foi lançada nova versão do Angular",
+    "description": "Angular atualiza para a nova versão 15 e traz novas funcionalidades.",
     "photoCover": "https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36",
   },
   {
     "id":"2",
-    "title": "TypeScript é difícil?",
+    "title": "O que é TypeScript?",
     "description": "TypeScript é uma linguagem de programação voltada para a criação de sites.",
     "photoCover": "https://miro.medium.com/v2/resize:fit:1200/0*myzb7qL_EWSX1QiH.png",
   },
@@ -17,7 +17,7 @@ export const dataFake = [
     "id":"3",
     "title": "O que é um Danco de Dados?",
     "description": "O Banco de Dados é onde todas as informações das empresas são armazenadas e manipuladas. Existem banco de dados SQL e NoSQL.",
-    "photoCover": "https://logusti.com.br/wp-content/uploads/2022/06/treinamento-sql.jpg",
+    "photoCover": "https://static.vecteezy.com/ti/fotos-gratis/t2/12830352-varios-bancos-de-dados-sao-colocados-em-tabelas-de-bancos-de-dados-relacionais-com-sala-de-servidores-e-plano-de-fundo-do-datacenter-conceito-de-servidor-de-banco-de-dados-sql-armazenamento-de-dados-design-de-diagrama-de-banco-de-dados-ilustracao-3d-foto.jpg",
   },
   {
     "id":"4",
