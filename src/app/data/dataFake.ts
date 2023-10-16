50ea3ab540a6d4d9cdaa43d9fe237e73afb45357
KasnorDKS <junior.castro10@hotmail.com>
2023-10-16 16:12:13 -0300

    Content Created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+// Banco de dados Falso
+
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "FRAMEWORK ANGULAR NOVA VERSÃO",
+    "description": "Angular atualiza para a nova versão 15 e trás novas funcionalidades.",
+    "photoCover": "https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36",
+  },
+  {
+    "id":"2",
+    "title": "TypeScript é difícil?",
+    "description": "TypeScript é uma linguagem de programação voltada para a criação de sites.",
+    "photoCover": "https://miro.medium.com/v2/resize:fit:1200/0*myzb7qL_EWSX1QiH.png",
+  },
+  {
+    "id":"3",
+    "title": "O que é um Danco de Dados?",
+    "description": "O Banco de Dados é onde todas as informações das empresas são armazenadas e manipuladas. Existem banco de dados SQL e NoSQL.",
+    "photoCover": "https://logusti.com.br/wp-content/uploads/2022/06/treinamento-sql.jpg",
+  },
+  {
+    "id":"4",
+    "title": "Programe de maneira fácil com Python",
+    "description": "Python é uma linguagem de programação com uma sintaxe simples, fácil para programadores iniciantes começarem à \"codar\".",
+    "photoCover": "https://www.panggi.com/images/featured/python.png",
+  },
+
+]
